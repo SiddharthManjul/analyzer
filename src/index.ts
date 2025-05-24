cee26bec91c00472acc8f88d69a491554ab5ac91
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-05-24 22:15:21 +0530

    Mistral Initialization

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import { ChatGroq } from "@langchain/groq";
+import { SystemMessage, HumanMessage, AIMessage } from "@langchain/core/messages";
+
+export const initializeMistralModel = async () => {
+    return new ChatGroq({
+        model: "mistral-saba-24b",
+        temperature: 0.1,
+        apiKey: process.env.GROQ_API_KEY,
+    });
+};
+
+export const getMIstralResponse = async (
+    model: ChatGroq,
+    chatHistory: (SystemMessage | HumanMessage | AIMessage)[]
+) => {
+    return await model.invoke(chatHistory)
+}
+
+export const initializeMistralChatHistory = () => [
+    new SystemMessage("You are a helpful assistant. Answer the user's questions to the best of your ability."),
+];
